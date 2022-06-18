7b693d33bb5866ec892457ec923515f59c0dd9af
doglas anjos <doglasquerino@gmail.com>
2022-06-17 22:27:35 -0300

    adicionando redis

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,13 +1,18 @@
+# mysite/asgi.py
 import os
-from channels.routing import ProtocolTypeRouter
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-django_asgi_app = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
